54df0a54b51fc81be037a9c8e900eeaff6256193
phyber <phyber@localhost>
2012-03-15 12:26:15 +0000

    whitelist.py: More work on the configuration.

--- python/whitelist.py
+++ python/whitelist.py
@@ -5,6 +5,59 @@ SCRIPT_LICENSE	= "GPL3"
 SCRIPT_DESC	= "Block private messages from people not on your whitelist."
 SCRIPT_COMMAND	= SCRIPT_NAME
 
+WHITELIST_CONFIG = {
+	"whitelists": {
+		'channels': {
+			"type":			"string",
+			"desc":			"Whitelisted channels",
+			"min":			0,
+			"max":			0,
+			"string_values":	"",
+			"default":		"",
+			"value":		"",
+			"check_cb":		"",
+			"change_cb":		"",
+			"delete_cb":		"",
+		},
+		'hosts': {
+			"type":			"string",
+			"desc":			"Whitelisted hosts",
+			"min":			0,
+			"max":			0,
+			"string_values":	"",
+			"default":		"",
+			"value":		"",
+			"check_cb":		"",
+			"change_cb":		"",
+			"delete_cb":		"",
+		},
+		'networks': {
+			"type":			"string",
+			"desc":			"Whitelisted networks",
+			"min":			0,
+			"max":			0,
+			"string_values":	"",
+			"default":		"",
+			"value":		"",
+			"check_cb":		"",
+			"change_cb":		"",
+			"delete_cb":		"",
+		},
+		'nicks': {
+			"type":			"string",
+			"desc":			"Whitelisted nicks",
+			"min":			0,
+			"max":			0,
+			"string_values":	"",
+			"default":		"",
+			"value":		"",
+			"check_cb":		"",
+			"change_cb":		"",
+			"delete_cb":		"",
+		},
+	},
+}
+
 try:
 	import weechat
 except:
@@ -31,11 +84,6 @@ def parse_message(server, signal_data):
 
 	return details
 
-def whitelist_config_get_value(section_name, option_name):
-	section = weechat.config_search_section(config_file, section_name)
-	value = weechat.config_search_option(config_file, section, option_name)
-
-	return value
 
 def whitelist_config_init():
 	config_file = weechat.config_new("whitelist", "whitelist_config_reload_cb", "")
@@ -43,11 +91,34 @@ def whitelist_config_init():
 		return
 	config_section = {}
 
-	for s in ('channels', 'hosts', 'networks', 'nicks'):
-		config_section[s] = weechat.config_new_section(config_file, s, 0, 0, "", "", "", "", "", "", "", "", "", "")
-		if not config_section[s]:
+	for section in WHITELIST_CONFIG:
+		#weechat.prnt("", "Section: {}".format(section))
+		config_section[section] = weechat.config_new_section(
+			config_file,
+			section,
+			0, 0, "", "", "", "", "", "", "", "", "", ""
+		)
+		if not config_section[section]:
 			weechat.config_free(config_file)
 			return
+		for option_name, props in WHITELIST_CONFIG[section].items():
+			#weechat.prnt("", "Option: {}".format(option_name))
+			weechat.config_new_option(
+				config_file,
+				config_section[section],
+				option_name,
+				props['type'],
+				props['desc'],
+				props['string_values'],
+				props['min'],
+				props['max'],
+				props['default'],
+				props['default'],
+				0,
+				props['check_cb'], "",
+				props['change_cb'], "",
+				props['delete_cb'], ""
+			)
 
 	return config_file
 
@@ -60,9 +131,15 @@ def whitelist_config_write(config_file):
 def whitelist_config_reload_cb(userdata, config_file):
 	return weechat.WEECHAT_CONFIG_READ_OK
 
+def whitelist_config_get_value(section_name, option_name):
+	section = weechat.config_search_section(config_file, section_name)
+	option = weechat.config_search_option(config_file, section, option_name)
+	value = weechat.config_string(option)
+
+	return value
+
 def whitelist_check(server, nick, host):
 	return False
-
 
 def whitelist_privmsg_modifier_cb(userdata, modifier, server, raw_irc_msg):
 	details = parse_message(server, raw_irc_msg)
@@ -79,7 +156,9 @@ def whitelist_privmsg_modifier_cb(userdata, modifier, server, raw_irc_msg):
 		return raw_irc_msg
 
 def whitelist_list():
-	pass
+	for type in WHITELIST_CONFIG['whitelists']:
+		value = whitelist_config_get_value('whitelists', type)
+		weechat.prnt("", "{}: {}".format(type, value))
 
 def whitelist_add(type, arg):
 	pass
@@ -87,13 +166,17 @@ def whitelist_add(type, arg):
 def whitelist_del(type, arg):
 	pass
 
-def whitelist_cmd(userdata, buffer, args):
-	weechat.prnt("", "UD: '{}' / BUF: '{}' / ARGS: '{}'".format(userdata, weechat.buffer_get_string(buffer, "name"), args))
+def whitelist_cmd(userdata, buffer, arg):
+	if arg in ('', 'list'):
+		whitelist_list()
+	
+	#weechat.prnt("", "UD: '{}' / BUF: '{}' / ARGS: '{}'".format(userdata, weechat.buffer_get_string(buffer, "name"), arg))
 	return weechat.WEECHAT_RC_OK
 
 if __name__ == '__main__':
 	if weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_DESC, "", ""):
 		config_file = whitelist_config_init()
+		whitelist_config_read(config_file)
 		version = weechat.info_get("version_number", "") or 0
 		weechat.hook_modifier("irc_in_privmsg", "whitelist_privmsg_modifier_cb", "")
 		weechat.hook_command(SCRIPT_COMMAND, "Manage the whitelist", "ARGS", "ARGS DESC", "", "whitelist_cmd", "")
